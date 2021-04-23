a377889715c28a183c4d60167d69b2575e77da61
Renan <renan.automation@gmail.com>
2021-04-23 18:38:19 -0300

    Adicionei um Welcome

---
+++ Cadastro/Entities/Department.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cadastro.Entities
+{
+    class Department
+    {
+        public string Name { get; set; }
+
+        public Department()
+        {
+
+        }
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
